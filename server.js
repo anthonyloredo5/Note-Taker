505af18c744a2fb86aebdba52332680953cb88ff
Anthony Loredo <anthonyloredo5@gmail.com>
2021-01-26 21:58:15 -0500

    changed get response

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("*", function (req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
